89ceb6b862af2633c6cfc8d2397d8fc83f4557f9
kshitij-y <kshitijyadav2003@gmail.com>
2025-05-24 01:35:38 +0530

    fixed spelling

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -4,7 +4,7 @@ export default function SplashScreen() {
   return (
     <div className="fixed inset-0 flex flex-col justify-center items-center z-50 animate-fadeOut delay-1500 duration-1000 ease-out">
       <h1 className="text-6xl font-extrabold text-red-700 select-none mb-2 font-sans">
-        Claneder
+        Calendar
       </h1>
       <p className="text-red-700 font-light text-lg select-none tracking-wide">
         - by kshitij
